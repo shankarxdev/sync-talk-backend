d899127ca1adfbf83db236d28ca4bd967eb7a463
Shankar Chakraborty <shankarxdev@gmail.com>
2026-01-11 00:04:44 -0500

    feat: implement basic websocket chat

---
+++ src/main/java/com/synctalk/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.synctalk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Author: Shankar Chakraborty
+ * Date: 2026-01-10
+ * Time: 11:35 p.m.
+ */
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
