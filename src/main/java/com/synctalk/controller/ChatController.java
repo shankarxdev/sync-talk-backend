8af8c307a4999787fb9495a0bba06be03ea05ad4
Shankar Chakraborty <shankarxdev@gmail.com>
2026-01-20 01:11:26 -0500

    configuration for postgres & flyway

---
+++ src/main/java/com/synctalk/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.synctalk.controller;
+
+import com.synctalk.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: Shankar Chakraborty
+ * Date: 2026-01-10
+ * Time: 11:42 p.m.
+ */
+
+
+@RestController
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(ChatMessage message){
+        message.setContent(message.getSender() + " joined.");
+        return message;
+    }
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
